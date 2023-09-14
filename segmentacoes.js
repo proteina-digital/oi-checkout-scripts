b8b6a99d9ad3ac01291c193b2d9f3596409b1a8b
willa kiane <willa_abreu@outlook.com>
2023-09-13 23:54:21 -0300

    Update segmentacoes.js

--- segmentacoes.js
+++ segmentacoes.js
@@ -3,6 +3,7 @@ const segmentacoes_precos = [
     {
         nome: "regular",
         produtos: [
+            { nome: 100, preco: "79,90", mais_vendido: false, popup: true, banner: false },
             { nome: 400, preco: "109,90", mais_vendido: false, popup: true, banner: false },
             { nome: 500, preco: "119,90", mais_vendido: true, popup: false, banner: true },
         ],
@@ -10,6 +11,7 @@ const segmentacoes_precos = [
     {
         nome: "combate1",
         produtos: [
+            { nome: 100, preco: "79,90", mais_vendido: false, popup: true, banner: false },
             { nome: 400, preco: "99,90", mais_vendido: false, popup: true, banner: false },
             { nome: 500, preco: "109,90", mais_vendido: true, popup: false, banner: true },
         ],
@@ -17,6 +19,7 @@ const segmentacoes_precos = [
     {
         nome: "combate2",
         produtos: [
+            { nome: 100, preco: "79,90", mais_vendido: false, popup: true, banner: false },
             { nome: 400, preco: "99,90", mais_vendido: false, popup: true, banner: false },
             { nome: 500, preco: "119,90", mais_vendido: true, popup: false, banner: true },
         ],
@@ -24,6 +27,7 @@ const segmentacoes_precos = [
     {
         nome: "combate3",
         produtos: [
+            { nome: 100, preco: "79,90", mais_vendido: false, popup: true, banner: false },
             { nome: 500, preco: "99,90", mais_vendido: true, popup: true, banner: true },
             { nome: 600, preco: "109,90", mais_vendido: false, popup: false, banner: false },
         ],
@@ -31,6 +35,7 @@ const segmentacoes_precos = [
     {
         nome: "lowcost",
         produtos: [
+            { nome: 100, preco: "79,90", mais_vendido: false, popup: true, banner: false },
             { nome: 400, preco: "99,90", mais_vendido: false, popup: true, banner: false },
             { nome: 500, preco: "109,90", mais_vendido: true, popup: false, banner: true },
         ],
@@ -38,6 +43,7 @@ const segmentacoes_precos = [
     {
         nome: "regularunificado",
         produtos: [
+            { nome: 100, preco: "79,90", mais_vendido: false, popup: true, banner: false },
             { nome: 400, preco: "109,90", mais_vendido: false, popup: true, banner: false },
             { nome: 500, preco: "119,90", mais_vendido: true, popup: false, banner: true },
         ],
@@ -45,6 +51,7 @@ const segmentacoes_precos = [
     {
         nome: "priceupunificado",
         produtos: [
+            { nome: 100, preco: "79,90", mais_vendido: false, popup: true, banner: false },
             { nome: 400, preco: "119,90", mais_vendido: true, popup: true, banner: true },
             { nome: 500, preco: "129,90", mais_vendido: false, popup: false, banner: false },
         ],
