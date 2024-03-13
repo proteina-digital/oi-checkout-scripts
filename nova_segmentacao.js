6131221ff55c9685830cbf86b4326e0433127104
eduardorph <eduardorph@hotmail.com>
2024-03-13 11:16:47 -0300

    Retirando o destaque do banner do 700 mega

--- nova_segmentacao.js
+++ nova_segmentacao.js
@@ -36,7 +36,7 @@ const nova_segmentacao_precos = [
 	        { nome: 500, preco: "89,90", mais_vendido: false, popup: true, banner: false },
 	        { nome: 600, preco: "109,90", mais_vendido: true, popup: false, banner: false },
 	        { nome: 6001, preco: "149,90", mais_vendido: false, popup: false, banner: true },
-	        { nome: 700, preco: "169,90", mais_vendido: false, popup: false, banner: true },
+	        { nome: 700, preco: "169,90", mais_vendido: false, popup: false, banner: false },
 	        { nome: 1, preco: "199,90", mais_vendido: false, popup: false, banner: false }
 	    ],
 	}
