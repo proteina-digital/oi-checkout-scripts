ca6a067a35368d916374362be46a7779776b76e7
eduardorph <eduardorph@hotmail.com>
2024-07-10 18:07:57 -0300

    Inclusão de validador de CPF

--- script-dados-pessoais.js
+++ script-dados-pessoais.js
@@ -1,6 +1,57 @@
 var Webflow = Webflow || [];
 const search = window.location.search;
 var cpf_valido = false;
+
+function validaCPF(cpf) {
+  var Soma = 0
+  var Resto
+
+  var strCPF = String(cpf).replace(/[^\d]/g, '')
+  
+  if (strCPF.length !== 11)
+     return false
+  
+  if ([
+    '00000000000',
+    '11111111111',
+    '22222222222',
+    '33333333333',
+    '44444444444',
+    '55555555555',
+    '66666666666',
+    '77777777777',
+    '88888888888',
+    '99999999999',
+    ].indexOf(strCPF) !== -1)
+    return false
+
+  for (i=1; i<=9; i++)
+    Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+
+  Resto = (Soma * 10) % 11
+
+  if ((Resto == 10) || (Resto == 11)) 
+    Resto = 0
+
+  if (Resto != parseInt(strCPF.substring(9, 10)) )
+    return false
+
+  Soma = 0
+
+  for (i = 1; i <= 10; i++)
+    Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i)
+
+  Resto = (Soma * 10) % 11
+
+  if ((Resto == 10) || (Resto == 11)) 
+    Resto = 0
+
+  if (Resto != parseInt(strCPF.substring(10, 11) ) )
+    return false
+
+  return true
+}
+
 function consulta_cpf(cpf) {
     $.ajax({
         dataType: "json",
@@ -483,7 +534,7 @@ $('[data-id]').on('click', function() {
                     expre = !validEmail(val);
                     break;
                 case "cpf":
-                    expre = val.length !== 11;
+                    expre = !validaCPF(val);
                     break;
                 case "data_nascimento":
                     expre = !/^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i.test(item.val());
@@ -495,6 +546,7 @@ $('[data-id]').on('click', function() {
             if (expre) {
                 item.focus();
                 item.css("border-color", "red");
+                item[0].scrollIntoView({ behavior: 'smooth', block: 'center' });
                 invalid = true;
                 console.log(input_name, expre);
             }
